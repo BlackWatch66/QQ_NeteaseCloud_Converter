5222a7aa5af7cd4a7d3137277178ef5c92df5877
blackwatch66 <blackwatch233@protonmail.com>
2023-06-14 10:52:43 +0800

    feat: 去除对wget的依赖

--- QQDownload/download.py
+++ QQDownload/download.py
@@ -2,21 +2,11 @@ import wget
 import subprocess
 import re
 
-def download (data,cookie=""):
+
+def download(data, cookie=""):
     fileName = data["name"] + "+--+" + data["singer"] + data["type"]
     rstr = r"[\/\\\:\*\?\"\<\>\|\ ]"  # '/ \ : * ? " < > |'
     fileName = re.sub(rstr, "_", fileName)  # 替换为下划线
     print("现在开始下载" + fileName + "...")
-    print(
-        "wget" + " \"" + data["url"] + "\""
-        " --referer y.qq.com" +
-        " -O " + fileName 
-    )
-    subprocess.call(
-    [
-        "wget" + " \"" + data["url"] + "\""
-        " --referer y.qq.com" +
-        " -O " + fileName
-    ], shell=True
-    )
+    wget.download(data["url"], out=fileName, referer="y.qq.com")
     return fileName
